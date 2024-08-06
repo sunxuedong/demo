0d6ec2148bdecf95aa4bc9da73738f485a692657
孙雪冬 <sunxd@hite.com.cn>
2024-08-06 11:02:21 +0800

    add testing case to parse

---
+++ packages/utils/test/parse/index.test.js
@@ -0,0 +1,13 @@
+import { tryParse } from "@/parse";
+
+describe("testing parse", () => {
+  describe("testing tryParse method", () => {
+    test("testing try parse successfully", () => {
+      expect(tryParse('{"a":1,"b":2}')).toEqual({ a: 1, b: 2 });
+    });
+
+    test("testing try parse unsuccessfully", () => {
+      expect(tryParse("{a: 1, b: 2}", {})).toEqual({});
+    });
+  });
+});
