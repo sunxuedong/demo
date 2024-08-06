4804b89d54706764f8d71fcad9ef9fe688ed29b4
孙雪冬 <sunxd@hite.com.cn>
2024-08-06 11:00:00 +0800

    update joinArray method to array

--- packages/utils/src/array/index.js
+++ packages/utils/src/array/index.js
@@ -2,8 +2,13 @@ export function getArray(data, defaultValue = []) {
   return Array.isArray(data) ? data : defaultValue;
 }
 
-export function joinArray({ data, symbol = ",", filterEmpty = false } = {}) {
-  data = getArray(data);
+export function joinArray({
+  data,
+  defaultValue = [],
+  symbol = ",",
+  filterEmpty = false,
+} = {}) {
+  data = getArray(data, defaultValue);
 
   if (filterEmpty) {
     data = data.filter(item => item !== "" && item != null);
