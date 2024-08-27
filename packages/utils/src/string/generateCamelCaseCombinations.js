39bd7de175bdaf501ecaea652116fc9d03c47c2f
孙雪冬 <sunxd@hite.com.cn>
2024-08-27 19:40:47 +0800

    update generateCamelCaseCombinations

--- packages/utils/src/string/generateCamelCaseCombinations.js
+++ packages/utils/src/string/generateCamelCaseCombinations.js
@@ -4,9 +4,9 @@ export default function generateCamelCaseCombinations({ data = [] }) {
   const results = [];
 
   // Generate combinations starting from the first word
-  for (let i = 0; i <= data.length; i++) {
+  for (let i = 1; i <= data.length; i++) {
     const subArray = data.slice(0, i);
-    results.push(toCamelCase(subArray));
+    results.push(toCamelCase({ data: subArray }));
   }
 
   return results;
