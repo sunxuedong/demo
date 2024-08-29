9a845445bb556615ded1337e1a78785aef5d958e
孙雪冬 <sunxd@hite.com.cn>
2024-08-29 18:22:56 +0800

    export timeout module in utils

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -4,4 +4,5 @@ export * from "./src/object/index";
 export * from "./src/observer/index";
 export * from "./src/parse/index";
 export * from "./src/string/index";
+export * from "./src/timeout/index";
 export * from "./src/tree/index";
